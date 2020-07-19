b08c31c1a7f22b641015c630284f74f4b7158c41
xenobarbital <greencross.am@gmail.com>
2020-07-19 16:03:22 +0400

    Finish navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,45 +1,95 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React, { useState } from 'react';
+import { Menu, Button, Drawer } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
 import logo from '../../assets/img/logo.png';
 import * as S from './styles';
 
-const { SubMenu, ItemGroup } = Menu;
+const LeftMenu = () => {
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item>
+        <a href="">Features</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="">Use Cases</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="">Pricing</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="">Docs</a>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+const RightMenu = () => {
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item className="li-condensed">
+        <Button type="primary">Start Free Trial</Button>
+      </Menu.Item>
+      <Menu.Item className="li-condensed">
+        <Button>Log In</Button>
+      </Menu.Item>
+    </Menu>
+  );
+}
 
 export const NavBar = () => {
+  const [displayDrawer, setDisplayDrawer] = useState(false);
+
+  const openDrawer = () => setDisplayDrawer(true);
+
+  const closeDrawer = () => setDisplayDrawer(false);
+
   return (
     <S.NavStyled>
       <S.LogoWrapper>
         <S.Logo src={logo} alt="logo" />
       </S.LogoWrapper>
-      <S.MenuCon>
+      <S.MenuCont>
         <S.MenuLeft>
-          <Menu mode="horizontal">
-            <Menu.Item key="mail">
-              <a href="">Features</a>
-            </Menu.Item>
-            <Menu.Item key="mail">
-              <a href="">Use Cases</a>
-            </Menu.Item>
-            <Menu.Item key="mail">
-              <a href="">Pricing</a>
-            </Menu.Item>
-            <Menu.Item key="mail">
-              <a href="">Docs</a>
-            </Menu.Item>
-          </Menu>
+          <LeftMenu />
         </S.MenuLeft>
 
         <S.MenuRight>
-          <Menu mode="horizontal">
-            <Menu.Item key="mail">
+          <RightMenu />
+        </S.MenuRight>
+
+        <S.BurgerCont>
+          <Button type="text" onClick={openDrawer}>
+            <MenuOutlined />
+          </Button>
+        </S.BurgerCont>
+        <Drawer
+          placement="right"
+          closable
+          onClose={closeDrawer}
+          visible={displayDrawer}
+        >
+          <Menu mode="vertical">
+            <Menu.Item>
               <a href="">Features</a>
             </Menu.Item>
-            <Menu.Item key="mail">
+            <Menu.Item>
               <a href="">Use Cases</a>
             </Menu.Item>
+            <Menu.Item>
+              <a href="">Pricing</a>
+            </Menu.Item>
+            <Menu.Item>
+              <a href="">Docs</a>
+            </Menu.Item>
+            <Menu.Item className="li-condensed">
+              <Button type="primary">Start Free Trial</Button>
+            </Menu.Item>
+            <Menu.Item className="li-condensed">
+              <Button>Log In</Button>
+            </Menu.Item>
           </Menu>
-        </S.MenuRight>
-      </S.MenuCon>
+        </Drawer>
+      </S.MenuCont>
     </S.NavStyled>
   );
 }
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import { Button, Drawer } from "antd";
 
 export const NavStyled = styled.nav`
   padding: 20px;
   height: 35px;
   @media (min-width: 768px) {
-    padding: 20px 40px;
+    padding: 20px 20px;
   }
   @media (min-width: 992px) {
     padding: 20px 80px;
@@ -29,15 +30,42 @@ export const Logo = styled.img`
   width: 100px;
 `;
 
-export const MenuCon = styled.div`
+export const MenuCont = styled.div`
   width: calc(100% - 150px);
   float: left;
 `;
 
 export const MenuLeft = styled.div`
   float: left;
+  @media (max-width: 767px) {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    padding-left: 100px;
+  }
+  @media (min-width: 1200px) {
+    padding-left: 150px;
+  }
+  @media (min-width: 1600px) {
+    padding-left: 300px;
+  }
 `;
 
 export const MenuRight = styled.div`
   float: right;
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+export const BurgerCont = styled.div`
+  float: right;
+  height: 47px;
+  margin-right: -50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: 768px) {
+    display: none;
+  }
 `;
